8c9b5229732a43ad755c2a6a13f626af98676345
Luis Acosta <acosta.luism6@gmail.com>
2023-07-08 12:16:47 +0000

    Write test

--- tests/Application/UseCases/GetCourseDetailTest.php
+++ tests/Application/UseCases/GetCourseDetailTest.php
@@ -2,10 +2,26 @@
 
 namespace App\Tests\Application\UseCases;
 
+use App\Application\UseCases\GetCourseDetail;
+use App\Domain\Course;
+use App\Domain\CourseRepository;
+
 class GetCourseDetailTest extends \PHPUnit\Framework\TestCase
 {
     public function testGetCourseDetail(): void
     {
-        $this->markTestIncomplete();
+        $courseRepository = $this->getMockBuilder(CourseRepository::class)
+                            ->getMock();
+        
+        $mockCourse = new Course(1, "Curso 1", "Descripción de prueba", 99.99, []);
+
+        $courseRepository->method("search")->willReturn($mockCourse);
+
+        $getCourseDetail = new GetCourseDetail($courseRepository);
+
+        $course = $getCourseDetail(1);
+
+        $this->assertInstanceOf(Course::class, $course);
+        $this->assertEquals($mockCourse, $course);
     }
 }
